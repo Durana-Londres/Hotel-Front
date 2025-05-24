1b53a89505be3a78acb1e3674444b6becd69ece8
Durana-Londres <duranalondres21@gmail.com>
2025-05-24 13:06:11 +0100

    comit

--- JS/cadastro.js
+++ JS/cadastro.js
@@ -1,11 +1,16 @@
- document.getElementById("form").addEventListener("submit", function(e) {
-      e.preventDefault(); 
+ document.getElementById("form").addEventListener("submit", function (e) {
+const senha = document.getElementById("senha").value;
+ const confirmar = document.getElementById("verif").value;
+const mensagem = document.getElementById("mensagem");
 
-      let senha = document.getElementById("senha").value;
-      let confirmar = document.getElementById("verif").value;
-      let mensagem = document.getElementById("mensagem");
-
-      if (senha !== confirmar) {
-        mensagem.textContent = "As senhas não coincidem.";
-        mensagem.style.color = "red";}
+   if(senha.length < 8){
+    e.preventDefault();
+    mensagem.textContent = "a senha deve conter no minímo 8 caratecres";
+   } else if (senha !== confirmar) {
+     e.preventDefault();
+     mensagem.textContent = "As senhas não coincidem.";
+      } else {
+    mensagem.textContent = ""; 
+      }
     });
+
