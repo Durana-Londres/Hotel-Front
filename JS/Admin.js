4484819b9cc0702707dd48dff4f05898132844b0
Durana-Londres <duranalondres21@gmail.com>
2025-05-25 23:13:20 +0100

    .

--- JS/Admin.js
+++ JS/Admin.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function abrirModal() {
  document.getElementById("meuModal").style.display = "flex";
 }
@@ -6,14 +5,3 @@ function abrirModal() {
  function fecharModal() {
    document.getElementById("meuModal").style.display = "none";
  }
-=======
-// Função para abrir o modal, exibindo-o na tela
-function abrirModal() {
-  document.getElementById("meuModal").style.display = "flex";
-}
-
-// Função para fechar o modal, ocultando-o da tela
-function fecharModal() {
-  document.getElementById("meuModal").style.display = "none";
-}
->>>>>>> d4340f0d870c05ae141ca3d704ad49647a6ec1f6
